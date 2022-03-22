eb8f3cc746e6f3fa98733c97e4cedfae7cd9c7a5
bzuker <brianzuker@gmail.com>
2022-03-22 18:23:21 -0300

    fixes

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,40 +1,44 @@
-import PropTypes from "prop-types";
 import Tab from '../components/Tab'
 import Image from 'next/image'
-import { NextPage } from "next";
+import React from 'react'
 
-
-// Props interface
 interface Props {
-    selectedTab: string;
-    setSelectedTab: Function;
-  }
+  selectedTab: string
+  setSelectedTab: Function
+}
 
-const Header: NextPage<Props> = ({selectedTab, setSelectedTab}) => (
-    <>
-    <nav className="root bg-header flex flex-row items-center border-gray-200 md:mx-12 lg:mx-12 md:mt-5 lg:mt-5 px-2 md:px-12 lg:px-12  py-5">
-        <Image height="50px" width='50px' src='/logo.svg'/>
-        <div className="align-middle text-xs md:text-m lg:text-xl px-8 lg:px-24 text-white">
-          <Tab content='Home' setSelectedTab={setSelectedTab} isSelected={selectedTab == 'Home'} />
-          <Tab content='Vaults' setSelectedTab={setSelectedTab} isSelected={selectedTab == 'Vaults'} />
-          <Tab content='Docs' setSelectedTab={setSelectedTab} isSelected={selectedTab == 'Docs'} />
-
-        </div>
-        <button className="ml-auto w-fit font-zen-dots text-primary text-xs lg:text-m border border-primary rounded-xl background-transparent py-4 px-4" type="button">
-          Connect
-        </button>
-      </nav>
-      <div className='root bg-banner text-white flex flex-row items-center border-gray-200 md:mx-12 lg:mx-12 md:mb-5 lg:mb-5 px-2 md:px-12 lg:px-12  py-2'>
-        this is a beta product, use at your own risk
+const Header: React.FC<Props> = ({ selectedTab, setSelectedTab }) => (
+  <>
+    <nav className="root flex flex-row items-center border-gray-200 bg-header px-2 py-5 md:mx-12 md:mt-5 md:px-12 lg:mx-12 lg:mt-5  lg:px-12">
+      <Image height="50px" width="50px" src="/logo.svg" />
+      <div className="md:text-m px-8 align-middle text-xs text-white lg:px-24 lg:text-xl">
+        <Tab
+          content="Home"
+          setSelectedTab={setSelectedTab}
+          isSelected={selectedTab === 'Home'}
+        />
+        <Tab
+          content="Vaults"
+          setSelectedTab={setSelectedTab}
+          isSelected={selectedTab === 'Vaults'}
+        />
+        <Tab
+          content="Docs"
+          setSelectedTab={setSelectedTab}
+          isSelected={selectedTab === 'Docs'}
+        />
       </div>
-      </>
-    
-  
+      <button
+        className="lg:text-m background-transparent ml-auto w-fit rounded-xl border border-primary py-4 px-4 font-zen-dots text-xs text-primary"
+        type="button"
+      >
+        Connect
+      </button>
+    </nav>
+    <div className="root flex flex-row items-center border-gray-200 bg-banner px-2 py-2 text-white md:mx-12 md:mb-5 md:px-12 lg:mx-12 lg:mb-5  lg:px-12">
+      this is a beta product, use at your own risk
+    </div>
+  </>
 )
-
-Header.propTypes = {
-    selectedTab: PropTypes.string.isRequired,
-    setSelectedTab: PropTypes.func.isRequired,
-  };
 
 export default Header
--- components/Tab.tsx
+++ components/Tab.tsx
@@ -1,20 +1,19 @@
 import classNames from 'classnames'
-import { NextPage } from "next";
+import React from 'react'
 
-// Props interface
 interface Props {
-    content: string;
-    isSelected: boolean;
-    setSelectedTab: Function;
-  }
+  content: string
+  isSelected: boolean
+  setSelectedTab: Function
+}
 
-
-const Tab: NextPage<Props> = ({content, isSelected, setSelectedTab}) => (
-    <button onClick={() => setSelectedTab(content)} className={classNames("pr-4 lg:pr-24", {"text-darkgrey": !isSelected})}>
-        {content}
-    </button>
-  
+const Tab: React.FC<Props> = ({ content, isSelected, setSelectedTab }) => (
+  <button
+    onClick={() => setSelectedTab(content)}
+    className={classNames('pr-4 lg:pr-24', { 'text-darkgrey': !isSelected })}
+  >
+    {content}
+  </button>
 )
-
 
 export default Tab
