fb5e37121f2a4f7deefa78f26a9fee1b8d8b02a2
Brian Zuker <brianzuker@gmail.com>
2022-03-31 11:23:00 -0300

    Feat/deposits (#4)
    * my deposits page
    
    * mock of all pages

--- components/Tab.tsx
+++ components/Tab.tsx
@@ -1,19 +1,20 @@
 import classNames from 'classnames'
+import Link from 'next/link'
 import React from 'react'
 
 interface Props {
-  content: string
+  content: 'Home' | 'Vaults' | 'Docs'
   isSelected: boolean
-  setSelectedTab: Function
 }
 
-const Tab: React.FC<Props> = ({ content, isSelected, setSelectedTab }) => (
-  <button
-    onClick={() => setSelectedTab(content)}
-    className={classNames('pr-4 lg:pr-24', { 'text-darkgrey': !isSelected })}
-  >
-    {content}
-  </button>
+const Tab: React.FC<Props> = ({ content, isSelected }) => (
+  <Link href={`/${content.toLowerCase()}`}>
+    <button
+      className={classNames('pr-4 lg:pr-24', { 'text-darkgrey': !isSelected })}
+    >
+      {content}
+    </button>
+  </Link>
 )
 
 export default Tab
